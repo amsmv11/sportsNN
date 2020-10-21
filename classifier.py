b534497b1fc10cb2dd50a7f705a6928fb904d913
Antonio Vieira <amsmv11@gmail.com>
2020-10-21 18:18:00 +0100

    fix: classifier

--- classifier.py
+++ classifier.py
@@ -1,21 +1,21 @@
 import pandas as pd
-from keras.models import Sequencial
+from keras.models import Sequential
 from keras.layers import Dense
 
 #importar os dataframes 
-# X_train = pd.read_cvs("nomedofile.cvs", header=None)  X=inputs
-# Y_train = pd.read_cvs("nomedofile.cvs", header=None)  Y=results
-# X_test = pd.read_cvs("nomedofile.cvs", header=None)
-# Y_test = pd.read_cvs("nomedofile.cvs", header=None)
+X_train = pd.read_csv("./breast_cancer_examples/xtrain.csv", header=None)  #X=inputs
+Y_train = pd.read_csv("./breast_cancer_examples/ytrain.csv", header=None)  #Y=results
+X_test = pd.read_csv("./breast_cancer_examples/xtest.csv", header=None)
+Y_test = pd.read_csv("./breast_cancer_examples/ytest.csv", header=None)
 
 
 #initialize the ANN
-classifier = Sequencial()
+classifier = Sequential()
 
 classifier.add(Dense(units = 16, activation="relu", input_dim=30)) #supondo que ha 30 features, ir reduzindo ate ter 3 outputs se o Ytrain tiver tres colunas
 classifier.add(Dense(units = 8, activation="relu"))
 classifier.add(Dense(units = 6, activation="relu"))
-classifier.add(Dense(units = 3, activation="sigmoid"))  #sigmoid vai obrigar a devolver valores entre 0 e 1
+classifier.add(Dense(units = 1, activation="sigmoid"))  #sigmoid vai obrigar a devolver valores entre 0 e 1
 
 classifier.compile(optimizer = "rmsprop", loss = "binary_crossentropy")
 
@@ -25,7 +25,7 @@ classifier.fit(X_train, Y_train, batch_size = 1, epochs = 100)
 
 #predicting
 Y_pred = classifier.predict(X_test)
-# Y_pred = [ 1 if y>=0.5 else 0 for y in Y_pred ]  ESTAO A TRANSFORMAR TODOS OS RESULTADOS EM 0 OU 1 PQ O Y_TEST DELES SO TEM ESSES RESULTADOS
+Y_pred = [ 1 if y>=0.5 else 0 for y in Y_pred ]  #ESTAO A TRANSFORMAR TODOS OS RESULTADOS EM 0 OU 1 PQ O Y_TEST DELES SO TEM ESSES RESULTADOS
 
 #ESTATISTICAS DOS RESULTADOS (mais uma vez depende se o Y (output) tem apenas uma coluna ou 3 )
 total = 0
@@ -40,4 +40,4 @@ for i in range(len(Y_pred)):
 
 print("Total: " + str(total))
 print("Correct: " + str(correct))
-print("Wrong: " + str(Wrong))
+print("Wrong: " + str(wrong))
