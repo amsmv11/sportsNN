0528a48761f7afc2d4d37bc54bec789a7aa9e8ba
Antonio Vieira <amsmv11@gmail.com>
2020-10-22 16:09:50 +0100

    add: importing and removing results from csv to dataframes

--- classifier.py
+++ classifier.py
@@ -3,10 +3,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 #importar os dataframes 
-X_train = pd.read_csv("./breast_cancer_examples/xtrain.csv", header=None)  #X=inputs
-Y_train = pd.read_csv("./breast_cancer_examples/ytrain.csv", header=None)  #Y=results
-X_test = pd.read_csv("./breast_cancer_examples/xtest.csv", header=None)
-Y_test = pd.read_csv("./breast_cancer_examples/ytest.csv", header=None)
+#X_train = pd.read_csv("./breast_cancer_examples/xtrain.csv", header=None)  #X=inputs
+#Y_train = pd.read_csv("./breast_cancer_examples/ytrain.csv", header=None)  #Y=results
+#X_test = pd.read_csv("./breast_cancer_examples/xtest.csv", header=None)
+#Y_test = pd.read_csv("./breast_cancer_examples/ytest.csv", header=None)
+
+ligaNOS2019 = pd.read_csv("./ligaNOS/portugal-liga-i-2018-to-2019.csv", header=None, low_memory=False)
+ligaNOS2020 = pd.read_csv("./ligaNOS/portugal-liga-i-2019-to-2020.csv", header=None, low_memory=False)
+
+ligaNOS2019Results = ligaNOS2019["FTR"]  #full time Result   H-home D-draw A-away
+ligaNOS2020Results = ligaNOS2020["FTR"]  #transformar em -1 0 1 e aplicar tanh?
+
+ligaNOS2019.drop("FTR", axis=1, inplace=True)
+ligaNOS2020.drop("FTR", axis=1, inplace=True)
+
 
 
 #initialize the ANN
