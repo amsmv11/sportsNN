fae58fe083d3c68536322bacb4955a0ac81e5307
Antonio Vieira <amsmv11@gmail.com>
2020-10-24 18:28:07 +0100

    add: start function processes results

--- classifier.py
+++ classifier.py
@@ -3,20 +3,21 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 #importar os dataframes 
-#X_train = pd.read_csv("./breast_cancer_examples/xtrain.csv", header=None)  #X=inputs
-#Y_train = pd.read_csv("./breast_cancer_examples/ytrain.csv", header=None)  #Y=results
-#X_test = pd.read_csv("./breast_cancer_examples/xtest.csv", header=None)
-#Y_test = pd.read_csv("./breast_cancer_examples/ytest.csv", header=None)
+# X_train = pd.read_csv("./breast_cancer_examples/xtrain.csv", header=None)  #X=inputs
+# Y_train = pd.read_csv("./breast_cancer_examples/ytrain.csv", header=None)  #Y=results
+# X_test = pd.read_csv("./breast_cancer_examples/xtest.csv", header=None)
+# Y_test = pd.read_csv("./breast_cancer_examples/ytest.csv", header=None)
 
-ligaNOS2019 = pd.read_csv("./ligaNOS/portugal-liga-i-2018-to-2019.csv", header=None, low_memory=False)
-ligaNOS2020 = pd.read_csv("./ligaNOS/portugal-liga-i-2019-to-2020.csv", header=None, low_memory=False)
+ligaNOS2019 = pd.read_csv("./ligaNOS/portugal-liga-i-2018-to-2019.csv", low_memory=False)
+ligaNOS2020 = pd.read_csv("./ligaNOS/portugal-liga-i-2019-to-2020.csv", low_memory=False)
 
 ligaNOS2019Results = ligaNOS2019["FTR"]  #full time Result   H-home D-draw A-away
-ligaNOS2020Results = ligaNOS2020["FTR"]  #transformar em -1 0 1 e aplicar tanh?
+ligaNOS2020Results = ligaNOS2020["FTR"]  
+
+
 
 ligaNOS2019.drop("FTR", axis=1, inplace=True)
 ligaNOS2020.drop("FTR", axis=1, inplace=True)
-
 
 
 #initialize the ANN
@@ -25,10 +26,9 @@ classifier = Sequential()
 classifier.add(Dense(units = 16, activation="relu", input_dim=30)) #supondo que ha 30 features, ir reduzindo ate ter 3 outputs se o Ytrain tiver tres colunas
 classifier.add(Dense(units = 8, activation="relu"))
 classifier.add(Dense(units = 6, activation="relu"))
-classifier.add(Dense(units = 1, activation="sigmoid"))  #sigmoid vai obrigar a devolver valores entre 0 e 1
+classifier.add(Dense(units = 3, activation="sigmoid"))  #sigmoid vai obrigar a devolver valores entre 0 e 1
 
 classifier.compile(optimizer = "rmsprop", loss = "binary_crossentropy")
-
 
 #training
 classifier.fit(X_train, Y_train, batch_size = 1, epochs = 100)
@@ -51,3 +51,13 @@ for i in range(len(Y_pred)):
 print("Total: " + str(total))
 print("Correct: " + str(correct))
 print("Wrong: " + str(wrong))
+print("Accuracy: " + str(correct/total)) 
+
+
+
+#FUNCOES AUX
+# def transform_results(dataFrame):
+#     resultsHome = []
+#     resultsTie = []
+#     resultsAway = []
+#     for e in dataFrame:
